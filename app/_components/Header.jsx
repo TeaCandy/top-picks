9094bbbe9231b772c549d6c10ee7105237359544
Candace Ryan <ryan.candace26@gmail.com>
2024-04-11 14:17:07 -0500

    small changes to navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
     //  </div> first try on logo sizing. alternative:
      <div className='flex justify-between items-center p-6 md:px-20 shadow-md fixed w-full top-0 left-0 z-20'>
         <Image src='/toppicks.png' alt='logo'
-        width={125}
+        width={150}
         height={200} />
 
             <div className='hidden md:flex border p-2 rounded-lg bg-gray-200 w-96'>
--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Order Here</h2>
-      <Button>Take 2</Button>
+      <h2>Candace's Capstone Prj</h2>
+      
 
       <UserButton afterSignOutUrl="/" />
     </div>
