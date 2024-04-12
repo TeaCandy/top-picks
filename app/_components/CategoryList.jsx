1e817c734516883b6812b79601077fb0f3222789
Candace Ryan <ryan.candace26@gmail.com>
2024-04-12 14:10:58 -0500

    made icons viewable but not scrollable

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -1,10 +1,11 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import GlobalApi from '../_utils/GlobalApi';
+import Image from 'next/image';
 
 function CategoryList() {
 
-    const [CategoryList,setCategoryList]=useState([]);
+    const [categoryList,setCategoryList]=useState([]);
     useEffect(()=>{
         getCategoryList();
       },[])
@@ -20,7 +21,18 @@ function CategoryList() {
         })
       }
   return (
-    <div>CategoryList</div>
+    <div>
+      <div>
+        {categoryList&&categoryList.map((category,index)=>(
+          <div key={index}>
+              <Image src={category.icon?.url} alt={category.name}
+              width={40}
+              height={40}
+              />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
