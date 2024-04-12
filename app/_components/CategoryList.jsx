e85e6937e2f1aaad64ba1115b0a693c04b55581e
Candace Ryan <ryan.candace26@gmail.com>
2024-04-12 17:29:18 -0500

    added keep hover effect to selected category

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -3,11 +3,20 @@ import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../_utils/GlobalApi';
 import Image from 'next/image';
 import { ArrowRightCircle } from 'lucide-react';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 function CategoryList() {
 
     const listRef=useRef(null)
     const [categoryList,setCategoryList]=useState([]);
+    const params=useSearchParams();
+    const [selectedCategory,setSelectedCategory]=useState('all')
+
+    useEffect(()=>{
+      setSelectedCategory(params.get('category'));
+    },[params])
+
     useEffect(()=>{
         getCategoryList();
       },[])
@@ -36,16 +45,18 @@ function CategoryList() {
     <div className='mt-10 relative'>
       <div className='flex gap-4 overflow-auto scrollbar-hide' ref={listRef}>
         {categoryList&&categoryList.map((category,index)=>(
-          <div key={index} 
-          className='flex flex-col items-center
-           gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-pink-50 cursor-pointer group'>
+          <Link href={'?category='+category.slug} key={index} 
+          className={`flex flex-col items-center
+           gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-pink-50 cursor-pointer group
+           ${selectedCategory==category.slug&&'text-primary border-primary bg-pink-50'}
+           `}>
               <Image src={category.icon?.url} alt={category.name}
               width={50}
               height={40}
               className='group-hover:scale-125 transition-all duration-200'
               />
               <h2 className='text-sm font-medium group-hover:text-primary'>{category.name}</h2>
-          </div>
+          </Link>
         ))}
       </div>
       <ArrowRightCircle className='absolute -right-10 top-9
