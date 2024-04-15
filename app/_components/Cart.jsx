e62f42d8f8ae4a70417335a56bc65c232c3d133e
Candace Ryan <ryan.candace26@gmail.com>
2024-04-15 01:38:20 -0500

    added remove cart component

--- app/_components/Cart.jsx
+++ app/_components/Cart.jsx
@@ -1,10 +1,15 @@
+
 import { Button } from '@/components/ui/button'
 import { X } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useContext } from 'react'
+import GlobalApi from '../_utils/GlobalApi'
+import { toast } from 'sonner'
+import { CartUpdateContext } from '../_context/CartUpdateContext'
 
 function Cart({cart}) {
 
+    const {updateCart,setUpdateCart}=useContext(CartUpdateContext);
     const CalculateCartAmount=()=>{
         let total=0;
         cart.forEach((item)=>{
@@ -12,7 +17,22 @@ function Cart({cart}) {
         })
         return total.toFixed(2);
     }
-  return (
+  
+    const RemoveItemFromCart=(id)=>{
+        GlobalApi.DisconnectRestroFromUserCartItem(id).then(resp=>{
+            console.log(resp);
+            if(resp)
+            {
+                GlobalApi.DeleteItemFromCart(id).then(resp=>{
+                    console.log(resp);
+                    toast('Item Removed!');
+                    setUpdateCart(!updateCart)
+                })
+            }
+        })  
+    }
+
+    return (
     <div>
         <h2 className='text-lg font-bold'>{cart[0]?.restaurant?.name}</h2>
         <div className='mt-5 flex flex-col gap-3'>
@@ -30,7 +50,9 @@ function Cart({cart}) {
                 <h2 className='text-sm'>{item?.productName}</h2>
                 </div>
                 <h2 className='font-bold flex gap-2'>${item.price}
-                <X className='h-4 w-4 text-red-700 '/>
+                <X className='h-4 w-4 text-red-700 '
+                onClick={()=>RemoveItemFromCart(item.id)}
+                />
                 </h2>
             </div>
             ))}
